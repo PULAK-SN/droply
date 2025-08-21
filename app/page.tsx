8a599b0a17c61a8307990e87d8c280d11a2d5f80
PULAK-SN <pulak.official22@gmail.com>
2025-08-21 18:32:43 +0530

    SignUp, clerk,OTP, zod and react-hook-form

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import SignUpForm from "@/components/SignUpForm";
+
 export default function Home() {
   return (
     <div className="">
       <h1>Droply</h1>
+      {/* <SignUpForm /> */}
     </div>
   );
 }
